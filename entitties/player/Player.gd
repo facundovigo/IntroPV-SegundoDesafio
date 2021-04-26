cb9c0a202c6084abf00c9815f4c712f9eeaf2687
facundo <facundovigo@gmail.com>
2021-04-25 21:44:03 -0300

    se sube parte del codigo

--- entitties/player/Player.gd
+++ entitties/player/Player.gd
@@ -1,9 +1,18 @@
 extends Sprite
 
+onready var arm = $Arm
+
 export (float) var speed: float
 
-func _process(delta):
+func initialize(projectile_container):
+	arm.container = projectile_container
 	
+func _physics_process(delta):
+	var mouse_position:Vector2 = get_local_mouse_position()
+	arm.rotation = mouse_position.normalized().angle()
+	#var new_projectile = projectile_scene.instance()
+	if Input.is_action_just_pressed("fire"):
+	 arm.fire()
 	var direction:int =  int(Input.is_action_pressed("derecha")) - int(Input.is_action_pressed("izquierda"))
 	position.x += direction * speed * delta
 
